ca3c50716f0b4ccc935bc95e32e534aa264eb080
LouisTsai <q1030176@gmail.com>
2024-01-06 15:23:53 +0800

    test: complete lendmixer testing configuration

---
+++ lendmixer/test/lending/lendMixer.t.sol
@@ -0,0 +1,74 @@
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ProxyAdmin } from "../../src/proxy/proxyAdmin.sol";
+import { Proxy } from "../../src/proxy/proxy.sol";
+import { LendMixer } from "../../src/lending/lendMixer.sol";
+import { AssetToken } from "../../src/lending/asset.sol";
+import { ILendMixer } from "../../src/interface/ILendMixer.sol";
+import { IERC3156FlashBorrower } from "../../src/interface/IERC3156FlashBorrower.sol";
+
+contract LendMixerTest is Test {
+
+    ProxyAdmin internal proxyAdmin;
+    Proxy internal proxy;
+    LendMixer internal lendMixer;
+    ERC20 internal token;
+
+    address internal user;
+    address internal owner;
+
+    event ProxyAdmin__UpgradeImplementation(address payable proxy, address implementation);
+    event ProxyAdmin__ChangeProxyAdmin(address payable proxy, address admin);
+    
+    event LendMixer__UpdateAssetTokenConfig(ERC20 token, AssetToken assetToken, bool permission);
+    event LendMixer__FlashLoanService(address receiver, ERC20 token, uint256 amount);
+
+    function setUp() public {
+        // Role Creation
+        user = makeAddr("user");
+        owner = makeAddr("owner");
+
+        // ProxyAdmin Creation
+        vm.prank(owner);
+        proxyAdmin = new ProxyAdmin();
+        assertEq(proxyAdmin.owner(), owner);
+
+        // LendMixer Creation
+        lendMixer = new LendMixer();
+
+        // Proxy Creation
+        vm.startPrank(owner);
+        proxy = new Proxy();
+        proxy.changeAdmin(address(proxyAdmin));
+
+        vm.expectEmit(true, true, false, false);
+        emit ProxyAdmin__UpgradeImplementation(payable(proxy), address(lendMixer));
+        proxyAdmin.upgradeTo(payable(proxy), address(lendMixer));
+        assertEq(proxyAdmin.getProxyImplementation(payable(proxy)), address(lendMixer));
+        vm.stopPrank();
+
+        vm.prank(user);
+        string memory version = ILendMixer(address(proxy)).version();
+        assertEq(version, "1.0.0");
+
+        // Underlying Token Creation
+        token = new UnderlyingToken();
+    }
+
+
+}
+
+/// @title simple underlying token
+/// @notice underlying token used for lending protocol
+/// @dev follow oppenzeppelin implementation
+contract UnderlyingToken is ERC20 {
+
+    uint256 internal constant initialSupply = 1e40;
+
+    constructor() ERC20("Underlying Token", "UT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
