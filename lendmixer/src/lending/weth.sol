21dd2d5f5ac5e119576bef8f11bf4a29c759ca33
LouisTsai <q1030176@gmail.com>
2024-01-07 16:19:10 +0800

    feat: add weth default configuration

--- lendmixer/src/lending/weth.sol
+++ lendmixer/src/lending/weth.sol
@@ -10,5 +10,41 @@ interface IReceiver {
 }
 
 contract WETH is IERC20 {
+    /*//////////////////////////////////////////////////////////////
+                                ERROR
+    //////////////////////////////////////////////////////////////*/
+    error WETH__EtherTransferFailed();
+    error WETH__TokenTransferFailed();
+    error WETH__TokenApprovalFailed();
+    error WETH__BalanceNotEnough();
+    error WETH__AllowanceNotEnough();
+    error WETH__TokenNotSupport();
+    error WETH__FlashMintAmountExceed();
+    error WETH__FlashMintTotalExceed();
+    error WETH__FlashLoanNotReceived();
+    error WETH__FlashMintBalanceNotEnough();
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLE
+    //////////////////////////////////////////////////////////////*/
+    string public constant name   = "Wrapped Token";
+    string public constant symbol = "WETH";
+
+    bytes32 internal immutable onFlashLoanReturnValue = keccak256("ERC3156FlashBorrower.onFlashLoan");
+
+    mapping(address=>uint256) internal _balanceOf;
+    mapping(address=>mapping(address=>uint256)) internal _allowance;
+
+    uint256 flashMintAmount;
+
+    /*//////////////////////////////////////////////////////////////
+                                EVENT
+    //////////////////////////////////////////////////////////////*/
+    event WETH__FlashMint(address initiator, address receiver, uint256 amount);
+
+    /*//////////////////////////////////////////////////////////////
+                                CONSTUCTOR
+    //////////////////////////////////////////////////////////////*/
+    constructor() payable {}
 
 }
