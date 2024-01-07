de7e66a7bf9181fffa4e19970c211c5fcc8f952d
LouisTsai <q1030176@gmail.com>
2024-01-07 16:26:32 +0800

    test: add token transfer receiver example

--- lendmixer/test/lending/weth.t.sol
+++ lendmixer/test/lending/weth.t.sol
@@ -30,4 +30,49 @@ contract WETHTest is Test {
         deal(user, initialSupply);
     }
 
+contract TransferRecevier is Test {
+
+    WETH internal weth;
+    address internal owner;
+    constructor(WETH _weth) payable {
+        weth = _weth;
+        owner = msg.sender;
+    }
+    function onTokenTransfer(address from, uint256 amount, bytes calldata data) external returns(bool) {
+        require(msg.sender==address(weth), "Invalid Caller");
+        require(from==owner, "Invalid Initiator");
+        weth.withdraw(amount);
+        (address addr, bool result) = abi.decode(data, (address, bool));
+        (bool success, ) = addr.call{value: amount}("");
+        require(success, "Transfer Failed");
+        return result;
+    }
+
+    function onTokenApproval(address from, uint256 amount, bytes memory data) external returns(bool) {
+        require(msg.sender==address(weth), "Invalid Caller");
+        require(from==owner, "Invalid Initiator");
+        (address addr, bool result) = abi.decode(data, (address, bool));
+        weth.withdrawFrom(from, payable(this), amount);
+        (bool success, ) = addr.call{value: amount}("");
+        require(success, "Transfer Failed");
+        return result;
+    }
+
+    function onFlashLoan(
+        address initiator,
+        address token,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata data
+    ) external returns (bytes32) {
+        require(msg.sender==address(weth), "Invalid Caller");
+        require(initiator==owner, "Invalid initiator");
+        require(token==address(weth), "Token Not Support");
+        (address addr, bool action, bool result) = abi.decode(data, (address, bool, bool));
+        if(!action) weth.transfer(addr, amount+fee);
+        if(result) return keccak256("ERC3156FlashBorrower.onFlashLoan");
+        return keccak256("failed");
+    }
+
+    receive() external payable {}
 }
